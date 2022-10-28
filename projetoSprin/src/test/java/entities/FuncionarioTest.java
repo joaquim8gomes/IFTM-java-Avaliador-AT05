5cf8fe5b11a344cbe57e8c191d7891cfd73ad0fd
Joaquim Gomes <joaquimgomes2468@gmail.com>
2022-10-27 21:30:07 -0300

    Atualização da classe Funcionario Teste.

--- projetoSprin/src/test/java/entities/FuncionarioTest.java
+++ projetoSprin/src/test/java/entities/FuncionarioTest.java
@@ -1,21 +1,26 @@
 package entities;
 
+import org.assertj.core.api.Assertions;
+
 public class FuncionarioTest {
 	@Test
 	public void testarConstrutorAlunoComDadosValidosEAlunoAprovado() {
-		//cenário de teste
+		//https://github.com/brunoqp78/exemplo_Aluno/commit/b5fedb345fb3ae8cd569da03e62a456e53e0c7ac#diff-5ebac3133f82da24703d1f2f54884ed6b6ab65b0e7997eb034e523689728470c
+		//cenário de teste 03d - TDD Desenvolvimento
+		//1.212,00
+		//40horas trabalhadas
+		
 		String nomeValido = "Joaquim";
-		double notaValida = 8.9;
-		int frequenciaValida = 77;
-		boolean resultadoEsperado = true;
-		Aluno a; 
+		int horasTrabalhadasValido = 50;
+		double valorHoraValido = 10;
+
+		Funcionario func; 
 
 		//execução		
-		a = new Aluno(nomeValido, notaValida, frequenciaValida);
-		boolean resultadoObtido = a.estaAprovado();
-		String nomeObtido = a.getNome();
-		double notaObtida = a.getNota();
-		int frequenciaObtida = a.getFrequencia();
+		func = new Funcionario(nomeValido, horasTrabalhadasValido, valorHoraValido);
+		String nomeObtido = func.getNome();
+		int horasTrabalhadasObtido = (int) func.getHorasTrabalhadas();
+		double valorHoraObtido = func.getHorasTrabalhadas();
 
 		//comparação
 		Assertions.assertEquals(resultadoEsperado, resultadoObtido);
